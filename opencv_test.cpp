8fe10bdb059f83bc4ca309a6c522b854ed05a922
Bruce-zc <77948686+Bruce-zc@users.noreply.github.com>
2022-05-15 16:47:57 +0800

    Update opencv_test.cpp
    Support 3-channel colorful images.

--- opencv_test.cpp
+++ opencv_test.cpp
@@ -8,12 +8,33 @@ using namespace std;
 int main()
 {
     cout << "OpenCV Version: " << CV_VERSION << endl;
-    Mat img = imread("./cat.jpg", 0);
-    imshow("img", img);
-    waitKey();
-    Matrix<uchar> m = img;
-    Mat img1 = m.slice(0, m.getRow() / 2, 0, m.getColumn() / 2).toMat(0);
-    imshow("img1", img1);
+
+    Mat img1 = imread("./cat.jpg", IMREAD_GRAYSCALE);
+    imshow("img_gray", img1);
+
+    Matrix<uchar> m1 = img1;
+    Mat img1_after = m1.slice(m1.getRow() / 4, 3 * m1.getRow() / 4, m1.getColumn() / 4, 3 * m1.getColumn() / 4).toMat(0);
+    imshow("img1_after", img1_after);
+
+
+    Mat img2 = imread("./cat.jpg", IMREAD_COLOR);
+    imshow("img_color", img2);
+
+    vector<Mat> img2_channels;
+	split(img2, img2_channels);
+
+    Matrix<uchar> mB = img2_channels.at(0);
+    Mat img2_after_B = mB.slice(mB.getRow() / 4, 3 * mB.getRow() / 4, mB.getColumn() / 4, 3 * mB.getColumn() / 4).toMat(0);
+    Matrix<uchar> mG = img2_channels.at(1);
+    Mat img2_after_G = mG.slice(mG.getRow() / 4, 3 * mG.getRow() / 4, mG.getColumn() / 4, 3 * mG.getColumn() / 4).toMat(0);
+    Matrix<uchar> mR = img2_channels.at(2);
+    Mat img2_after_R = mR.slice(mR.getRow() / 4, 3 * mR.getRow() / 4, mR.getColumn() / 4, 3 * mR.getColumn() / 4).toMat(0);
+
+    Mat img2_after;
+    Mat img2_channels_after[3] = {img2_after_B, img2_after_G, img2_after_R};
+    merge(img2_channels_after, 3, img2_after);
+
+    imshow("img2_after", img2_after);
     waitKey();
     return 0;
 }
